eeb0edf3b6558a9c4bdcdc2bafffb42d865875b7
Joseph Liao <billan452211@gmail.com>
2025-02-01 17:09:16 -0500

    Analytic fix?

--- personal-website0v4/src/app/layout.js
+++ personal-website0v4/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,7 +25,7 @@ export default function RootLayout({ children }) {
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 {children}
-                <Analytics />
+                <Analytics></Analytics>
             </body>
         </html>
     );
